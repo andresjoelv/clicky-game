bb093200eb2ec15bb6761b18bb5a01762538250b
Nejo <andresjoelv@gmail.com>
2019-03-01 00:31:07 -0500

    Updates

---
+++ precache-manifest.0ca3992ea08cb22586d88d83c85a0b6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "65561bcd7297aa0aa22bd297dd4bb854",
+    "url": "/clicky-game/static/media/giphy_5.65561bcd.gif"
+  },
+  {
+    "revision": "56f5320b44fb0eb0dc0a620bb921ca48",
+    "url": "/clicky-game/static/media/giphy_3.56f5320b.gif"
+  },
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "1c98e767a68d17b108a0",
+    "url": "/clicky-game/static/js/main.1c98e767.chunk.js"
+  },
+  {
+    "revision": "5a1567e63549d6e40524",
+    "url": "/clicky-game/static/js/2.5a1567e6.chunk.js"
+  },
+  {
+    "revision": "1c98e767a68d17b108a0",
+    "url": "/clicky-game/static/css/main.f64a1230.chunk.css"
+  },
+  {
+    "revision": "817f83d7ee534eea49f69ec01ad42302",
+    "url": "/clicky-game/index.html"
+  }
+];
